349e0cb086c0b1a315ee89b1f5470370a003db18
Magda Saif <magdasaif3@gmail.com>
2022-01-02 13:03:23 +0200

    update priority

--- resources/views/pages/Sitesection/add.blade.php
+++ resources/views/pages/Sitesection/add.blade.php
@@ -33,31 +33,38 @@
             </button>
         </div>
         <div class="modal-body">
-            
+
             <form method="POST" action="{{route('site_section.store')}}" enctype="multipart/form-data">
-            
+
                 @csrf
                 {{-- <input name="_token" value="{{csrf_token()}}"> --}}
 
-              
-                
+
+
                 <div class="form-group">
                     <label for="site_name_ar">اسم القسـم بالعربيه</label>
-                    <input type="text" class="form-control" id="site_name_ar" aria-describedby="site_name_ar" placeholder="Enter name" name="site_name_ar" required>
+                    <input type="text" class="form-control" id="site_name_ar" aria-describedby="site_name_ar" placeholder="ادخل اسم القسـم بالعربيه" name="site_name_ar" required>
                     @error('site_name_ar')
                     <small class="form-text text-danger">{{$message}}</small>
                     @enderror
                 </div>
-            
+
                 <div class="form-group">
                     <label for="site_name_en">اسم القسم بالانجليزيه</label>
-                    <input type="text" class="form-control" id="site_name_en" aria-describedby="site_name_en" placeholder="Enter name" name="site_name_en" required>
+                    <input type="text" class="form-control" id="site_name_en" aria-describedby="site_name_en" placeholder="ادخل اسم القسـم بالانجليزية" name="site_name_en" required>
                     @error('site_name_en')
                     <small class="form-text text-danger">{{$message}}</small>
                     @enderror
                 </div>
 
-                
+                <div class="form-group">
+                    <label for="priority">الأولوية</label>
+                    <input type="text" class="form-control" id="priority" aria-describedby="priority" placeholder="ادخل الأولوية" name="priority" required>
+                    @error('priority')
+                    <small class="form-text text-danger">{{$message}}</small>
+                    @enderror
+                </div>
+
                 <div class="form-group">
                     <label for="image">صوره</label>
                     <input type="file" class="form-control" name="image">
@@ -74,7 +81,7 @@
                             <option value="0">غير مُفعل</option>
                     </select>
                 </div>
-                
+
                 <div class="modal-footer">
                         <button type="submit" class="btn btn-primary">اضافه</button>
                 </div>
