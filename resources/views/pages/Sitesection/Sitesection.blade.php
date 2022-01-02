1aca240262a495401d92fb468ad708441ecdaea7
Magda Saif <magdasaif3@gmail.com>
2022-01-02 13:02:25 +0200

    display priority

--- resources/views/pages/Sitesection/Sitesection.blade.php
+++ resources/views/pages/Sitesection/Sitesection.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.master')
 @section('css')
-@toastr_css
+
 @section('title')
-    اقسام الموقع
+أقسام الموقع
 @stop
 @endsection
 @section('page-header')
-<!-- breadcrumb -->
+
+
 @if(Session::has('success'))
 
     <div class="alert alert-success">
@@ -21,6 +22,7 @@
      </div>
 @endif
 
+<!-- breadcrumb -->
 <div class="page-title">
     <div class="row">
         <div class="col-sm-6">
@@ -42,84 +44,58 @@
     <div class="col-md-12 mb-30">
         <div class="card card-statistics h-100">
             <div class="card-body">
-         <!---------------------------- error msg------------------------------->
-                @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-       <!-----------------------------العرض-------------------------------->
-                <div class="container">
 
+            <!--#############################################################-->
+                    <div class="table-responsive">
 
-
-                    <div class="card-body" style="text-align: right;">
-                        <button type="button" class="btn btn-info" ><a href="{{route('site_section.create')}}">  اضافة قسم جديد</a></button>
-
-                        <br><br>
-                  <table class="table table-hover">
+                    <table id="datatable" class="table table-striped table-bordered p-0">
                     <thead>
-                      <tr>
+                        <tr  style="color: #17899b;" >
                         <th>#</th>
                         <th>اسم القسم</th>
                         <th>الصورة</th>
-                        <th>الترتيب</th>
+                        <th>الأولوية</th>
                         <th>الحالة</th>
                         <th>الاجراءات</th>
-                      </tr>
+                        </tr>
                     </thead>
                     <tbody>
-                        <?php $i = 0; $statues=1?>
-                       
+                         <?php $i = 0; $statues=1?>
+
                         @foreach($site_section as $section)
                             <tr>
                             <?php $i++; ?>
                             <td>{{ $i }}</td>
-                            <td>{{$section->site_name_ar}}</td> 
-                            <td><img  style="width: 90px; height: 90px;" src="<?php echo asset("storage/site_sections/site_section_image/$section->image")?>"></td>
-                           {{--  <td>{{$section->image}}</td>--}}
-                            <td>{{$i}}</td>
-                
+                            <td>{{$section->site_name_ar}}</td>
+
+
+                            <td><img  style="width: 90px; height: 90px;" src=<?php echo asset("storage/site_sections/{$section->image}")?> alt="" ></td>
+                            {{-- <td><img src="{{ public_path('images/$section->image')}}" /></td> --}}
+                            <td>{{$section->priority}}</td>
+
                             @if($section->statues==1)
-                            <td>مفعل</td>
+                            <td  style="color: green;font-size: 18px;">مفعل</td>
                             @else
-                            <td>غير مفعل</td>
+                            <td  style="color: red;font-size: 18px;">غير مفعل</td>
                             @endif
-                          
-                          {{--  <td> 
+
+                          {{--  <td>
                                  <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#edit{{ $section->id}}"
                                    title="تعديل">  تعديل </button>
                              </td>
 
                              --}}
-                             
+
                             <td> <button type="button" class="btn btn-info" ><a href={{route('site_section.edit',$section->id)}}> تعديل</a></button>
                              </td>
-                             
+
                             </tr>
-                
- <!--------------------------------- edit_modal----------------------------------->
-
-
-                          
                         @endforeach
-                      
+
                     </tbody>
-                  </table>
-                </div>
-                
-                
-                </div>
- 
-
-<!--------------------------------------------------------------------------------->
-
-
-
+                </table>
+            </div>
+            <!--#############################################################-->
 
             </div>
         </div>
@@ -128,6 +104,5 @@
 <!-- row closed -->
 @endsection
 @section('js')
-@toastr_js
-@toastr_render
+
 @endsection
