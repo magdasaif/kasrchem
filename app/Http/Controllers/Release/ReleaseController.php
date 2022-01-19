4c67db3cdd29be33e802329c8593a92a8da5066e
Magda Saif <magdasaif3@gmail.com>
2022-01-19 13:06:44 +0200

    Update app/Http/Controllers/Release/ReleaseController.php

--- app/Http/Controllers/Release/ReleaseController.php
+++ app/Http/Controllers/Release/ReleaseController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\ReleaseRequest;
 use App\Models\Release;
-use App\Models\Main_Category; 
+use App\Models\Main_Category;
 use App\Models\Sub_Category2;
 use App\Models\Sub_Category3;
 use App\Models\Sub_Category4;
@@ -42,20 +42,20 @@ class ReleaseController extends Controller
          $validated = $request->validated();
            if($request->image)
            {
-               
+
             $folder_name='release_'. $last_id;
               $photo_name= ($request->image)->getClientOriginalName();
                ($request->image)->storeAs($folder_name,$photo_name,$disk="release");
-               
+
            }
           if($request->file)
            {
              $folder_name='release_'. $last_id;
               $file_name= ($request->file)->getClientOriginalName();
                ($request->file)->storeAs($folder_name,$file_name,$disk="release");
-               
+
            }
-         
+
             $release = new Release
            ([
             'main_cate_id' =>  $request->main_category,
@@ -69,7 +69,7 @@ class ReleaseController extends Controller
             'file' =>$file_name,
            ]);
         $release->save();
-       
+
             return redirect()->route('release.index')->with(['success'=>'تمت الاضافه بنجاح']);
         }
         catch(\Exception $e){
@@ -91,9 +91,9 @@ class ReleaseController extends Controller
     public function update(ReleaseRequest $request, $id)
     {
     // dd($request->$release_id);
-    
+
         try {
-           
+
              $validated = $request->validated();
             // $rel = Release::findOrFail($request->id);
             $rel = Release::findOrFail($id);
@@ -104,42 +104,42 @@ class ReleaseController extends Controller
              $rel-> title_ar= $request->title_ar;
              $rel->title_en = $request->title_en;
              $rel-> status=$request->status;
- 
+
              if($request->image)
              {
               $validator = $request->validate(['image' => 'required|image|mimes:jpg,png,PNG,jpeg,gif,svg|max:2048',]);
-              if ($validator->fails()) { 
+              if ($validator->fails()) {
                    return redirect()->back()->with($validator->errorrs());
                 }
-  
+
 
            $folder_name='release_'.$id;
              $photo_name= ($request->image)->getClientOriginalName();
              ($request->image)->storeAs($folder_name,$photo_name,$disk="release");
-             
+
              $rel->image = $photo_name;
              }
-           
- 
+
+
              if($request->filee)
              {
-             
+
                 $validator=$request->validate(['file' => 'required|max:10000|mimes:application/pdf,application/vnd.openxmlformats-officedocument.wordprocessingml.document',]);
-             if ($validator->fails()) { 
+             if ($validator->fails()) {
                 return redirect()->back()->with($validator->errorrs());
             }
              $folder_name='release_'.$id;
              $file_name= ($request->filee)->getClientOriginalName();
              ($request->filee)->storeAs($folder_name,$file_name,$disk="release");
-             
+
              $rel->file = $file_name;
              }
-          
+
         $rel->save();
        return redirect()->route('release.index')->with(['success'=>'تم التعديل بنجاح']);
      }
      catch
-     (\Exception $e) 
+     (\Exception $e)
      {
          return redirect()->back()->with(['error' => $e->getMessage()]);
      }
@@ -149,10 +149,10 @@ class ReleaseController extends Controller
     public function destroy($id)
     {
         // dd($id);
-        try 
+        try
         {
-        $Release=Release::find($id);  
-        $Release->delete(); 
+        $Release=Release::find($id);
+        $Release->delete();
         return redirect()->route('release.index')->with(['success'=>'تم الحذف بنجاح']);
        }
        catch
@@ -164,7 +164,7 @@ class ReleaseController extends Controller
     //--------------------------------------------
     public function errorrs()
     {
-     
+
     return   [
         'image.required' =>'الملف مطلوب',
         'image.max' =>'يجب ان يكون المرفق لا يتعدى ال 10000',
