1d486d4ef9c105a2b0fb4f51b7f8cf7e5c84bcb1
YasmeenAli31 <engyasmeenali@gmail.com>
2022-02-14 12:48:50 +0200

    delete in article

--- resources/views/pages/Article/Show.blade.php
+++ resources/views/pages/Article/Show.blade.php
@@ -57,27 +57,19 @@
                             <td><img  style="width: 90px; height: 90px;" src=<?php echo asset("storage/article/{$article->image}")?> alt="" ></td>
                             <td >{{$article->title_ar}}</td>
                             <td style="font-weight: bold;font-size: 17px;" ><?php if($article->status==1){echo'<i class="fas fa-check green"></i>';}else{echo'<i class="fas fa-times red"></i>';}?></td>
-                            <!-- <td> 
-                             <button type="button" class="btn btn-info" ><a href="{{route('article.edit',$article->id)}}"  target="_blank"> تعديل</a></button>
-                             <button class="btn btn-danger" data-catid={{$article->id}} data-toggle="modal" data-target="#delete{{$article->id}}">حذف</button>
-                            </td> -->
+                           
                             <td style="font-weight: bold;font-size: 17px;">
 							<a href="{{route('article.edit',$article->id)}}"  title="تعديل"><i class="fa fa-edit blue"></i></a>
                             
 
-                            <!--/ <a  title="حذف" data-catid="{{$article->id}}" data-toggle="modal" data-target="#delete{{$article->id}}"> <i class="fa fa-trash red"></i></a> -->
-                            <!--  <button class="btn btn-danger" data-catid={{$article->id}} data-toggle="modal" data-target="#delete{{$article->id}}">حذف</button>-->
+                            / <a  title="حذف" data-catid="{{$article->id}}" data-toggle="modal" data-target="#delete{{$article->id}}"> <i class="fa fa-trash red"></i></a>
 
-
-                        </td>
-                            </tr>
-                        <!--############################ model for delete #################################-->
+                     <!--############################ model for delete #################################-->
           
-                            <div class="modal modal-danger fade" id="delete{{$article->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                    <div class="modal modal-danger fade" id="delete{{$article->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
                             <div class="modal-dialog" role="document">
                                 <div class="modal-content">
-                                <div class="modal-header" style="direction: ltr;">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                <div class="card-header" >
                                     <h4 class="modal-title " id="myModalLabel">تاكيد الحذف</h4>
                                 </div>
                                 <form action="{{route('article.destroy',$article->id)}}"  method="post">
@@ -94,12 +86,16 @@
 
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-danger" data-dismiss="modal">الغاء </button>
-                                        <button type="submit" class="btn btn-success">حذف</button>
+                                        <input type="submit" value="حذف"  class="btn btn-primary">
                                     </div>
                                 </form>
                                 </div>
                             </div>
                             </div>
+                              <!--#######################################################################################-->
+                        </td>
+                            </tr>
+                        
             
 
                         @endforeach
