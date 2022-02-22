8624395ca9e637a5b1bfe9b53a6129c03c7df0f0
YasmeenAli31 <engyasmeenali@gmail.com>
2022-02-22 11:31:43 +0200

    change upload_image in edit_product

--- public/js/product/edit_script.js
+++ public/js/product/edit_script.js
@@ -1,6 +1,6 @@
  //---------------for show selct option of when change on any one of them------------------------//
- $(document).ready(function () {
-
+$(document).ready(function () {
+ 
     $('select[name="section_id"]').on('change', function () {
     // alert('ssss');
     var section_id = $(this).val();
@@ -371,5 +371,20 @@ $('select[name="main_cate_id"]').on('change', function () {
                 alert('AJAX load did not work');
             }
         });
-    });
-    //--------------------------------------------------------------------------//
+        //--------------change upload image by clicking n image-----------------//
+          $('#previewImg').on('click', function () {
+           // alert("xxx");
+           jQuery('#my_file').trigger('click');
+         
+         
+           });
+       //--------------change upload image by clicking on button-------------------//
+
+           $('#btn_image').on('click', function () {
+             jQuery('#my_file').trigger('click');
+            });
+           
+    }); 
+    //--------------------------------------------------------------------------
+
+
