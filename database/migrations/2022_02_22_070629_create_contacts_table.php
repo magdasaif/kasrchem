8f2ae6be4c303372ddae8f463d21a575d585e114
s3 <git@murabba.com>
2022-02-23 23:31:19 +0200

    change cotact us token field from string into longtext

--- database/migrations/2022_02_22_070629_create_contacts_table.php
+++ database/migrations/2022_02_22_070629_create_contacts_table.php
@@ -18,7 +18,7 @@ class CreateContactsTable extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('phone');
-            $table->string('token');
+            $table->longText('token');
             $table->longText('message');
             $table->timestamps();
         });
