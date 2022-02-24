ab1e4b9c8b5507c622cf2af3441cf5a07e5fad3a
s3 <git@murabba.com>
2022-02-24 02:08:34 +0200

    clean up functions

--- app/Http/Controllers/Api/AboutUsController.php
+++ app/Http/Controllers/Api/AboutUsController.php
@@ -37,48 +37,6 @@ class AboutUsController extends Controller
     }
 
 
-    /**
-     * @OA\Get (
-     *      path="/setting",
-     *      operationId="Get site setting information",
-     *      tags={"Setting"},
-     *      summary="Get site setting information",
-     *      @OA\Parameter(
-     *          name="locale",
-     *          description="App Locale",
-     *          required=true,
-     *          in="header",
-     *          @OA\Schema(
-     *              type="string",
-     *              enum={"ar", "en"},
-     *              default="ar"
-     *          )
-     *      ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="Successful operation",
-     *          @OA\MediaType(
-     *           mediaType="application/json",
-     *      )
-     *      ),
-     *      @OA\Response(
-     *          response=401,
-     *          description="Unauthenticated",
-     *      ),
-     *      @OA\Response(
-     *          response=403,
-     *          description="Forbidden"
-     *      ),
-     *      @OA\Response(
-     *          response=400,
-     *          description="Bad Request"
-     *      ),
-     *      @OA\Response(
-     *          response=404,
-     *          description="not found"
-     *      ),
-     *     )
-     */
 
     public function setting(Request $request){
         //use header to read parameter passed in header
@@ -96,91 +54,12 @@ class AboutUsController extends Controller
         return response($setting,200,['OK']);
     }
 
-
-    /**
-     * @OA\Post(
-     *      path="/contact",
-     *      operationId="send_message",
-     *      tags={"Contact"},
-     *      summary="Contact Message Endpoint",
-     *      description="Send Message",
-     *      @OA\Parameter(
-     *          name="locale",
-     *          description="App Locale",
-     *          required=true,
-     *          in="header",
-     *          @OA\Schema(
-     *              type="string",
-     *              enum={"ar", "en"},
-     *              default="ar"
-     *          )
-     *      ),
-     *     @OA\RequestBody(
-     *          required=true,
-     *          @OA\MediaType(mediaType="multipart/form-data",
-     *              @OA\Schema(
-     *                  required={"name","email","phone","message","token"},
-     *                  @OA\Property(
-     *                      property="name",
-     *                      type="string",
-     *                      description="Sender Name"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="email",
-     *                      type="string",
-     *                      description="Sender Email"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="phone",
-     *                      type="string",
-     *                      description="Sender Phone"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="message",
-     *                      type="string",
-     *                      description="Message Content"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="token",
-     *                      type="string",
-     *                      description="form token"
-     *                  ),
-     *             )
-     *         )
-     *      ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="Successful operation",
-     *          @OA\MediaType(
-     *           mediaType="application/json",
-     *      )
-     *      ),
-     *      @OA\Response(
-     *          response=401,
-     *          description="Unauthenticated",
-     *      ),
-     *      @OA\Response(
-     *          response=403,
-     *          description="Forbidden"
-     *      ),
-     *      @OA\Response(
-     *          response=400,
-     *          description="Bad Request"
-     *      ),
-     *      @OA\Response(
-     *          response=404,
-     *          description="not found"
-     *      ),
-     *     )
      * @param ContactRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
-
     public function contact(Request $request){
-
             //use header to read parameter passed in header
              $lang=$request->header('locale');
-
              if($lang=='ar'){
                 $validation_message = [
                     'name.required' =>'اسم الراسل مطلوب',
@@ -192,7 +71,6 @@ class AboutUsController extends Controller
                     'phone.max' =>'أقصي حد لرقم الهاتف ١٤ رقم',
                     'phone.digits' =>'تاكد من ادخال 14 رقم فى الهاتف',
                     'message.required' =>'تاكد من ادخال محتوى الرساله',
-
                 ];
             }else{
                 $validation_message =[
@@ -205,22 +83,9 @@ class AboutUsController extends Controller
                     'phone.max' =>'Phone Number Could be 14 digits at most',
                     'phone.digits' =>'Be sure phone is 14 digit',
                     'message.required' =>'Message is requird',
-
                 ];
             }
-
-            //--------------to use trans fun ---but read site lang instead of passed lang----------------
-            // [
-            //     'name.required' => trans('contact.name'),
-            //     'email.required' => trans('contact.mail_requird'),
-            //     'email.email' =>trans('contact.mail_email'),
-            //     'phone.required' =>trans('contact.phone_requird'),
-            //     'phone.numeric' =>trans('contact.phone_numeric'),
-            //     'phone.digits' =>trans('contact.phone_digits'),
-            //     'message.required' =>trans('contact.message_requird'),
-            //     ]
         $response = array('response' => '', 'success'=>false);
-
         $validator = Validator::make(
             $request->all(),
             [
@@ -231,11 +96,9 @@ class AboutUsController extends Controller
            ],
           $validation_message
         );
-
         if ($validator->fails()) {
              $response['response'] = $validator->messages();
         } else {
-
             //insert into db
             $cont=new Contact();
             $cont->name =$request->name;
@@ -244,57 +107,9 @@ class AboutUsController extends Controller
             $cont->token =$request->token;
             $cont->message =$request->message;
             $cont->save();
-
-             //to email --->get mail of site owner to send mail to it
-//              $site_email= SiteInfo::get()->pluck('site_mail')->first();
-
-//            $response['success']=$site_email;
-
-            //send mail with the content (passed here) of contact blade in emails folder
-             //handel subject,from ,to in this mail
-//             Mail::send(
-//                 'emails.contact',
-//                 array(
-//                     'title' => 'Contact Mail -- تواصل معنا ',
-//                     'name' => $request->get('name'),
-//                     'mail' => $request->get('email'),
-//                     'phone' => $request->get('phone'),
-//                     'content' => $request->get('message'),
-//                 ),
-//                 function ($message) use ($request) {
-//                     $message->from($request->email);
-//                     $message->subject("Contact Mail -- تواصل معنا ");
-//                     $message->to( ($site_email) ? $site_email : 'eradunited@murabba.dev' );
-                    foreach ( SiteInfo::get()->pluck('site_mail') as $recipient) {
-                    Mail::to($recipient)->send(
-                                    array(
-                                        'title' => 'Contact Mail -- تواصل معنا ',
-                                        'name' => $request->get('name'),
-                                        'mail' => $request->get('email'),
-                                        'phone' => $request->get('phone'),
-                                        'content' => $request->get('message'),
-                                    ),
-                    );
-                    }
-//                    $message->to('eradunited@murabba.dev');
-                }
-            );
-
-
-            $response['response']='تم ارسال الرساله بنجاح';
-           // $response['success']='true';
-
-
-
-          //  Mail::To($to_email) ->send(new ConatctEmail($data));
-//             $response['success']= $to_email;
-
+            foreach ( SiteInfo::get()->pluck('site_mail') as $recipient) {
+                    Mail::to($recipient)->send(new ConatctEmail($cont));}
+            $response['response'] = 'تم ارسال الرساله بنجاح';
         }
-
         return $response;
-
-
-
-    }
-
 }
