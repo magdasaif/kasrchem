4850c99d49eb8b2f39950dc91dfad1afaeb55d95
Magda Saif <magdasaif3@gmail.com>
2022-03-22 20:04:11 +0200

    Update config/l5-swagger.php

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -150,7 +150,8 @@ return [
          */
         'constants' => [
         // 'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'https://backend.training.murabba.dev/api'),
-        'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST','https://backend.eradunited.training.murabba.dev/api'),  
+        // 'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST','https://backend.eradunited.training.murabba.dev/api'),
+        'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST','https://dashboard.eradunited.murabba.dev//api'),
 
     ],
     ],
