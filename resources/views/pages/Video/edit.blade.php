bfcd092d9311a8fcb57ab3b59edbabaaf1060bc6
YasmeenAli31 <engyasmeenali@gmail.com>
2022-03-23 15:49:58 +0200

    add sections in video adding,updated

--- resources/views/pages/Video/edit.blade.php
+++ resources/views/pages/Video/edit.blade.php
@@ -40,7 +40,55 @@
                 @csrf
                  <!--'video','Main_Cat','Sub_Category2','Sub_Category3','Sub_Category4'------------------------------------->
                <!----------------------------------------------------->
- 
+               <div class="form-group">
+                <label for="exampleInputEmail1">الأقسام</label> 
+                         <?php
+                        $selected_supplier=array();
+
+                        foreach ($video->rel_section as $supplier_select){
+                          array_push($selected_supplier,$supplier_select->id);
+                         }
+                         ?>
+                        
+                <select class="form-control" name="site_id[]"  multiple required oninvalid="this.setCustomValidity('اختر القسم')"  oninput="this.setCustomValidity('')" >
+                        
+                 @foreach ($sections as $sec)
+                 <?php
+                     $margin="0";
+                     $color="#c20620";
+                     $size="15";
+                     $type='supplier_section';
+                     $number=2;
+                     if(in_array($sec->id,$selected_supplier)){
+                         $select_or_no='selected';
+                     }else{
+                         $select_or_no='';
+                     }
+
+
+                    $new= [
+                         'childs' => $sec->childs,
+                         'margin'=>$margin+30,
+                         'color'=>'#209c41',
+                         'size'=>$size-1,
+                         'selected_supplier'=>$selected_supplier,
+                         'type'=>$type,
+                         'number'=>$number
+                     ];
+                 ?>
+                     <option style="margin-right:{{$margin}}px;color: {{$color}};font-size: {{$size}}px;" value="{{ $sec->id }}" <?php if (collect(old('site_id'))->contains($sec->id)) {echo 'selected';}else{echo $select_or_no;}?>> - {{ $sec->site_name_ar }}</option>
+                     @if(count($sec->childs))
+                         @include('pages.products.manageChild',$new)
+                     @endif
+                 @endforeach
+                 
+             </select>
+       
+            </div>
+            
+
+   
+            <!----------------------------------------------------->
                <!----------------------------------------------------->
               <!--========================================================-->
               
