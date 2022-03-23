5d9d91411a62bc5cc18783bbcd82a646a52e0304
unknown <magdasaif3@gmail.com>
2022-03-23 09:20:09 +0200

    change swigger link and handel preg_replace method on product resources

--- app/Http/Resources/SupplierSectionResource.php
+++ app/Http/Resources/SupplierSectionResource.php
@@ -23,6 +23,7 @@ class SupplierSectionResource extends JsonResource
             $all=Supplier::select('*','suppliers.id as id')
                             ->join('supplier_sections', 'supplier_sections.supplier_id', '=', 'suppliers.id')
                             ->where('supplier_sections.sitesection_id',$this->section_id)
+                            ->where('suppliers.parent_id',0)
                             ->get();
 
              $supplier=SupplierResource2::collection($all);
